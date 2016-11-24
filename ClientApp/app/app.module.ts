c8fbbff124fe04236b68c05cacd4773fca438a08
Jeff Bailey <jeff.bailey@itron.com>
2016-11-23 20:02:25 -0800

    Rid angularitics

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -11,10 +11,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { AboutComponent } from './components/about/about.component';
 import { LaserComponent } from './components/laser/laser.component';
 import { WifiComponent } from './components/wifi/wifi.component';
-import { Angulartics2Module, Angulartics2GoogleAnalytics } from 'angulartics2';
-import {enableProdMode} from '@angular/core';
 
-enableProdMode();
 @NgModule({
     bootstrap: [AppComponent],
     declarations: [
@@ -31,7 +28,7 @@ enableProdMode();
     ],
     imports: [
         UniversalModule, // Must be first import. This automatically imports BrowserModule, HttpModule, and JsonpModule too.,
-        Angulartics2Module.forRoot(),
+
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
@@ -44,8 +41,7 @@ enableProdMode();
             { path: 'wifi', component: WifiComponent },
             { path: '**', redirectTo: 'home' }
         ])
-    ],
-    providers: [Angulartics2GoogleAnalytics]
+    ]
 })
 export class AppModule {
 }
